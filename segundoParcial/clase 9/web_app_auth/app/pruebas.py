3a5ded2bbbc9adf996d43fa32e9a6178d0a1d4c0
saxel0360 <isaacjoelillatarco@gmail.com>
2024-05-15 14:19:38 -0100

    Avance 15/05/2024

--- segundoParcial/clase 9/web_app_auth/app/pruebas.py
+++ segundoParcial/clase 9/web_app_auth/app/pruebas.py
@@ -4,7 +4,7 @@ def my_decorador(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         print("Antes de llamar a la funcion")
-        result=(func(args[0].upper(),**kwargs))        
+        result=(func(args[0].upper(),**kwargs))
         print("Despues de llamar a la funcion")
         return result
     return wrapper
